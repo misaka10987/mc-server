5196a7de3f5e36ef86a4e391e3bd2a5efde17e80
misaka10987 <misaka10987@outlook.com>
2023-07-04 20:11:58 +0800

    Bugfix.

--- inst-mc.sh
+++ inst-mc.sh
@@ -27,7 +27,6 @@ esac
 
 mkdir -p "${HOME}/.mc-server/.java" "./mc-server"
 ln -s "${HOME}/.mc-server"
-mv "./.mc-server" "./mc-server"
 
 if [ -z "$java" ]; then
     getjava "${HOME}/.mc-server/.java" $java_version
